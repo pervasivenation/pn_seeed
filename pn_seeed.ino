9760f11e5852b3ad0368139758f400e2ce74f498
Diarmuid O'Coilean <diarmuidcire@gmail.com>
2020-02-14 11:25:27 +0000

    initial commit of Pervasive Nation IoT testbed Seeeduino LoRa toolkit.

---
+++ pn_seeed.ino
@@ -0,0 +1,84 @@
+
+#include <LoRaWan.h>
+
+
+//unsigned char buffer[128] = {0xef, 0xff, 0x55, 3, 4, 5, 6, 7, 8, 9,};
+unsigned char data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA,};
+char buffer[256];
+
+void setup(void)
+{
+    SerialUSB.begin(115200);
+    while(!SerialUSB);
+    
+    lora.init();
+
+    memset(buffer, 0, 256);
+
+    lora.getVersion(buffer, 256, 1);
+
+    SerialUSB.print(buffer); 
+
+    memset(buffer, 0, 256);
+    lora.getId(buffer, 256, 1);
+    SerialUSB.print(buffer);
+    
+    //lora.setId (DevAddr, DevEUI, AppEUI)
+    lora.setId("DA006039", "00DBBF7C763121C4", "2a7d151628bea2a6ace7158809de4f3b");
+ 
+    lora.setDeciveMode(LWOTAA);
+    lora.setDataRate(DR0, EU868);
+
+    lora.setChannel(0, 867.1);
+    lora.setChannel(1, 867.3);
+    lora.setChannel(2, 867.5);
+    lora.setChannel(3, 867.7);
+    lora.setChannel(4, 867.9);
+    lora.setChannel(5, 868.1);
+    lora.setChannel(6, 868.3);
+    lora.setChannel(7, 868.5);
+
+    lora.setReceiceWindowFirst(0, 867.1);
+    lora.setReceiceWindowSecond(868.5, DR3);
+    lora.setDutyCycle(false);
+    lora.setJoinDutyCycle(false);
+    lora.setPower(30);
+    while(!lora.setOTAAJoin(JOIN)){
+      SerialUSB.println("Trying to Join network");
+    }
+}
+
+void loop(void)
+{
+    bool result = false;
+
+    SerialUSB.println("Send string packet to Pervasive Nation NOC.");
+    //This sketch will broadcast a string to Pervasive Nation Network 
+    result = lora.transferPacket("SampleData!", 10);
+    
+    if(result)
+    {
+        short length;
+        short rssi;
+        SerialUSB.print("Result is: ");
+        memset(buffer, 0, 256);
+        length = lora.receivePacket(buffer, 256, &rssi);
+        if(length)
+        {
+            SerialUSB.print("Length is: ");
+            SerialUSB.println(length);
+            SerialUSB.print("RSSI is: ");
+            SerialUSB.println(rssi);
+            SerialUSB.print("Data is: ");
+            for(unsigned char i = 0; i < length; i ++)
+            {
+                SerialUSB.print("0x");
+                SerialUSB.print(buffer[i], HEX);
+                SerialUSB.print(" ");
+            }
+            SerialUSB.println();
+        }
+        SerialUSB.print("Sleep for 240000 ms (4minutes) ");
+        delay(240000);
+    }
+}
