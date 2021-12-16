a3648dbfa6b6a56cfededcff640b032fb6ad307a
diarmuid <dcollin5@tcd.ie>
2021-12-16 11:11:57 +0000

    added code to support Grove VOC and eCO2 to LoRaWAN Seeeduino Sensor

---
+++ pn_seeed_sgp30.ino/pn_seeed_sgp30.ino.ino
@@ -0,0 +1,133 @@
+#include <Arduino.h>
+#include <LoRaWan.h>
+#include "sensirion_common.h"
+#include "sgp30.h"
+
+
+//unsigned char buffer[128] = {0xef, 0xff, 0x55, 3, 4, 5, 6, 7, 8, 9,};
+unsigned char data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA,};
+char buffer[256];
+
+void setup(void)
+{
+    //SPG30 VOC and eCO2 gas sensor - end
+    s16 err;
+    u16 scaled_ethanol_signal, scaled_h2_signal;
+    Serial.begin(115200);
+    Serial.println("serial start!!");
+
+    /*For wio link!*/
+    #if defined(ESP8266)
+    pinMode(15, OUTPUT);
+    digitalWrite(15, 1);
+    Serial.println("Set wio link power!");
+    delay(500);
+    #endif
+    /*  Init module,Reset all baseline,The initialization takes up to around 15 seconds, during which
+        all APIs measuring IAQ(Indoor air quality ) output will not change.Default value is 400(ppm) for co2,0(ppb) for tvoc*/
+    while (sgp_probe() != STATUS_OK) {
+        Serial.println("SGP failed");
+        while (1);
+    }
+    /*Read H2 and Ethanol signal in the way of blocking*/
+    err = sgp_measure_signals_blocking_read(&scaled_ethanol_signal,
+                                            &scaled_h2_signal);
+    if (err == STATUS_OK) {
+        Serial.println("get ram signal!");
+    } else {
+        Serial.println("error reading signals");
+    }
+    err = sgp_iaq_init();
+    //
+    //SPG30 VOC and eCO2 gas sensor - end
+    
+    SerialUSB.begin(115200);
+    while(!SerialUSB);
+    
+    lora.init();
+
+    memset(buffer, 0, 256);
+
+    lora.getVersion(buffer, 256, 1);
+
+    SerialUSB.print(buffer); 
+
+    memset(buffer, 0, 256);
+    lora.getId(buffer, 256, 1);
+    SerialUSB.print(buffer);
+    
+    //lora.setId (DevAddr, DevEUI, AppEUI)
+    lora.setId("DA006039", "00DBBF7C763121C4", "2a7d151628bea2a6ace7158809de4f3b");
+ 
+    lora.setDeciveMode(LWOTAA);
+    lora.setDataRate(DR0, EU868);
+
+    lora.setChannel(0, 868.1);
+    lora.setChannel(1, 868.3);
+    lora.setChannel(2, 868.5);
+    lora.setChannel(3, 867.1);
+    lora.setChannel(4, 867.3);
+    lora.setChannel(5, 867.5);
+    lora.setChannel(6, 867.7);
+    lora.setReceiceWindowFirst(0, 868.1);
+    lora.setReceiceWindowSecond(869.5, DR3);
+
+    lora.setDutyCycle(false);
+    lora.setJoinDutyCycle(false);
+    lora.setPower(30);
+    while(!lora.setOTAAJoin(JOIN)){
+      SerialUSB.println("Trying to Join network");
+    }
+}
+
+void loop(void)
+{
+    bool result = false;
+
+    //SPG30 VOC and eCO2 gas sensor - beginning
+    s16 err = 0;
+    u16 tvoc_ppb, co2_eq_ppm;
+    err = sgp_measure_iaq_blocking_read(&tvoc_ppb, &co2_eq_ppm);
+    if (err == STATUS_OK) {
+        Serial.print("tVOC  Concentration:");
+        Serial.print(tvoc_ppb);
+        Serial.println("ppb");
+
+        Serial.print("CO2eq Concentration:");
+        Serial.print(co2_eq_ppm);
+        Serial.println("ppm");
+    } else {
+        Serial.println("error reading IAQ values\n");
+    }
+    //SPG30 VOC and eCO2 gas sensor - end
+    
+    SerialUSB.println("Send string packet to Pervasive Nation NOC.");
+    //This sketch will broadcast a string to Pervasive Nation Network 
+    result = lora.transferPacket("SampleData!", 10);
+    
+    if(result)
+    {
+        short length;
+        short rssi;
+        SerialUSB.print("Result is: ");
+        memset(buffer, 0, 256);
+        length = lora.receivePacket(buffer, 256, &rssi);
+        if(length)
+        {
+            SerialUSB.print("Length is: ");
+            SerialUSB.println(length);
+            SerialUSB.print("RSSI is: ");
+            SerialUSB.println(rssi);
+            SerialUSB.print("Data is: ");
+            for(unsigned char i = 0; i < length; i ++)
+            {
+                SerialUSB.print("0x");
+                SerialUSB.print(buffer[i], HEX);
+                SerialUSB.print(" ");
+            }
+            SerialUSB.println();
+        }
+        SerialUSB.print("Sleep for 240000 ms (4minutes) ");
+        delay(240000);
+    }
+}
