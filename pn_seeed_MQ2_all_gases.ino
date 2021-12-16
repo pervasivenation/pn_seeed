527a59c6136e56de71f7c6c9fce10d3b5d2e1295
diarmuid <dcollin5@tcd.ie>
2021-12-16 16:37:05 +0000

    mq2sensor all gases

---
+++ pn_seeed_MQ2_all_gases.ino
@@ -0,0 +1,284 @@
+
+#include <LoRaWan.h>
+
+/************************mq2sensor************************************/
+/************************Hardware Related Macros************************************/
+#define         MQ2PIN                       (0)     //define which analog input channel you are going to use
+#define         RL_VALUE_MQ2                 (1)     //define the load resistance on the board, in kilo ohms
+#define         RO_CLEAN_AIR_FACTOR_MQ2      (9.577)  //RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
+                                                     //which is derived from the chart in datasheet
+
+/***********************Software Related Macros************************************/
+#define         CALIBARAION_SAMPLE_TIMES     (50)    //define how many samples you are going to take in the calibration phase
+#define         CALIBRATION_SAMPLE_INTERVAL  (500)   //define the time interal(in milisecond) between each samples in the
+                                                     //cablibration phase
+#define         READ_SAMPLE_INTERVAL         (50)    //define how many samples you are going to take in normal operation
+#define         READ_SAMPLE_TIMES            (5)     //define the time interal(in milisecond) between each samples in 
+                                                     //normal operation
+
+/**********************Application Related Macros**********************************/
+#define         GAS_HYDROGEN                  (0)
+#define         GAS_LPG                       (1)
+#define         GAS_METHANE                   (2)
+#define         GAS_CARBON_MONOXIDE           (3)
+#define         GAS_ALCOHOL                   (4)
+#define         GAS_SMOKE                     (5)
+#define         GAS_PROPANE                   (6)
+#define         accuracy                      (0)   //for linearcurves
+//#define         accuracy                    (1)   //for nonlinearcurves, un comment this line and comment the above line if calculations 
+                                                    //are to be done using non linear curve equations
+/*****************************Globals************************************************/
+float           Ro = 0;                            //Ro is initialized to 10 kilo ohms
+
+
+//https://www.seeedstudio.com/Grove-Gas-Sensor-MQ2.html
+//https://docs.particle.io/assets/datasheets/electronsensorkit/MQ-2.pdf
+//https://github.com/mdsiraj1992/Gassensors/blob/master/arduino%20codes/mq2_sensor/mq2_sensor.ino
+
+//unsigned char buffer[128] = {0xef, 0xff, 0x55, 3, 4, 5, 6, 7, 8, 9,};
+unsigned char data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA,};
+char buffer[256];
+
+
+void setup(void)
+{
+    SerialUSB.begin(115200);
+
+    Serial.print("Calibrating...\n");                
+    Ro = MQCalibration(MQ2PIN);                       //Calibrating the sensor. Please make sure the sensor is in clean air 
+                                                    //when you perform the calibration                    
+    Serial.print("Calibration is done...\n"); 
+    Serial.print("Ro=");
+    Serial.print(Ro);
+    Serial.print("kohm");
+    Serial.print("\n");
+  
+    while(!SerialUSB);
+    
+    lora.init();
+
+    memset(buffer, 0, 256);
+
+    lora.getVersion(buffer, 256, 1);
+
+    SerialUSB.print(buffer); 
+
+    memset(buffer, 0, 256);
+    lora.getId(buffer, 256, 1);
+    SerialUSB.print(buffer);
+    
+    //lora.setId (DevAddr, DevEUI, AppEUI)
+    lora.setId("DA006039", "00DBBF7C763121C4", "2a7d151628bea2a6ace7158809de4f3b");
+ 
+    lora.setDeciveMode(LWOTAA);
+    lora.setDataRate(DR0, EU868);
+
+    lora.setChannel(0, 868.1);
+    lora.setChannel(1, 868.3);
+    lora.setChannel(2, 868.5);
+    lora.setChannel(3, 867.1);
+    lora.setChannel(4, 867.3);
+    lora.setChannel(5, 867.5);
+    lora.setChannel(6, 867.7);
+    lora.setReceiceWindowFirst(0, 868.1);
+    lora.setReceiceWindowSecond(869.5, DR3);
+
+    lora.setDutyCycle(false);
+    lora.setJoinDutyCycle(false);
+    lora.setPower(30);
+    while(!lora.setOTAAJoin(JOIN)){
+      SerialUSB.println("Trying to Join network");
+    }
+}
+
+void loop(void)
+{
+   Serial.print("HYDROGEN:"); 
+   Serial.print(MQGetGasPercentage(MQRead(MQ2PIN)/Ro,GAS_HYDROGEN) );
+   Serial.print( "ppm" );
+   Serial.print("    ");   
+   Serial.print("LPG:"); 
+   Serial.print(MQGetGasPercentage(MQRead(MQ2PIN)/Ro,GAS_LPG) );
+   Serial.print( "ppm" );
+   Serial.print("    ");   
+   Serial.print("METHANE:"); 
+   Serial.print(MQGetGasPercentage(MQRead(MQ2PIN)/Ro,GAS_METHANE) );
+   Serial.print( "ppm" );
+   Serial.print("    ");   
+   Serial.print("CARBON_MONOXIDE:"); 
+   Serial.print(MQGetGasPercentage(MQRead(MQ2PIN)/Ro,GAS_CARBON_MONOXIDE) );
+   Serial.print( "ppm" );
+   Serial.print("    ");   
+   Serial.print("ALCOHOL:"); 
+   Serial.print(MQGetGasPercentage(MQRead(MQ2PIN)/Ro,GAS_ALCOHOL) );
+   Serial.print( "ppm" );
+   Serial.print("    ");   
+   Serial.print("SMOKE:"); 
+   Serial.print(MQGetGasPercentage(MQRead(MQ2PIN)/Ro,GAS_SMOKE) );
+   Serial.print( "ppm" );
+   Serial.print("    ");   
+   Serial.print("PROPANE:"); 
+   Serial.print(MQGetGasPercentage(MQRead(MQ2PIN)/Ro,GAS_PROPANE) );
+   Serial.print( "ppm" );
+   Serial.print("\n");
+   delay(200);
+   
+   float sensor_volt;
+   float RS_air; // Get the value of RS via in a clear air
+   float R0; // Get the value of R0 via in H2
+    float RS_gas; // Get value of RS in a GAS
+    float ratio; // Get ratio RS_GAS/RS_air
+    int sensorValue = analogRead(A0);
+    sensor_volt=(float)sensorValue/1024*5.0;
+    RS_gas = (5.0-sensor_volt)/sensor_volt; // omit *RL
+    /*-Replace the name "R0" with the value of R0 in the demo of First Test -*/
+    ratio = RS_gas/R0; // ratio = RS/R0
+    /*-----------------------------------------------------------------------*/
+    Serial.print("sensor_volt = ");
+    Serial.println(sensor_volt);
+    Serial.print("RS_ratio = ");
+    Serial.println(RS_gas);
+    Serial.print("Rs/R0 = ");
+    Serial.println(ratio);
+    Serial.print("\n\n");
+    delay(1000);
+
+    bool result = false;
+
+    SerialUSB.println("Send string packet to Pervasive Nation NOC.");
+    //This sketch will broadcast a string to Pervasive Nation Netwo3rk 
+    result = lora.transferPacket("RS_ratio!", RS_gas);
+    
+    if(result)
+    {
+        short length;
+        short rssi;
+        SerialUSB.print("Result is: ");
+        memset(buffer, 0, 256);
+        length = lora.receivePacket(buffer, 256, &rssi);
+        if(length)
+        {
+            SerialUSB.print("Length is: ");
+            SerialUSB.println(length);
+            SerialUSB.print("RSSI is: ");
+            SerialUSB.println(rssi);
+            SerialUSB.print("Data is: ");
+            for(unsigned char i = 0; i < length; i ++)
+            {
+                SerialUSB.print("0x");
+                SerialUSB.print(buffer[i], HEX);
+                SerialUSB.print(" ");
+            }
+            SerialUSB.println();
+        }
+    }
+    SerialUSB.print("Sleep for 240000 ms (4minutes) ");
+    delay(240000);
+}
+
+/****************** MQResistanceCalculation ****************************************
+Input:   raw_adc - raw value read from adc, which represents the voltage
+Output:  the calculated sensor resistance
+Remarks: The sensor and the load resistor forms a voltage divider. Given the voltage
+         across the load resistor and its resistance, the resistance of the sensor
+         could be derived.
+************************************************************************************/ 
+float MQResistanceCalculation(int raw_adc)
+{
+  return ( ((float)RL_VALUE_MQ2*(1023-raw_adc)/raw_adc));
+}
+
+/***************************** MQCalibration ****************************************
+Input:   mq_pin - analog channel
+Output:  Ro of the sensor
+Remarks: This function assumes that the sensor is in clean air. It use  
+         MQResistanceCalculation to calculates the sensor resistance in clean air 
+         and then divides it with RO_CLEAN_AIR_FACTOR. RO_CLEAN_AIR_FACTOR is about 
+         10, which differs slightly between different sensors.
+************************************************************************************/ 
+float MQCalibration(int mq_pin)
+{
+  int i;
+  float RS_AIR_val=0,r0;
+
+  for (i=0;i<CALIBARAION_SAMPLE_TIMES;i++) {                     //take multiple samples
+    RS_AIR_val += MQResistanceCalculation(analogRead(mq_pin));
+    delay(CALIBRATION_SAMPLE_INTERVAL);
+  }
+  RS_AIR_val = RS_AIR_val/CALIBARAION_SAMPLE_TIMES;              //calculate the average value
+
+  r0 = RS_AIR_val/RO_CLEAN_AIR_FACTOR_MQ2;                      //RS_AIR_val divided by RO_CLEAN_AIR_FACTOR yields the Ro 
+                                                                 //according to the chart in the datasheet 
+
+  return r0; 
+}
+
+/*****************************  MQRead *********************************************
+Input:   mq_pin - analog channel
+Output:  Rs of the sensor
+Remarks: This function use MQResistanceCalculation to caculate the sensor resistenc (Rs).
+         The Rs changes as the sensor is in the different consentration of the target
+         gas. The sample times and the time interval between samples could be configured
+         by changing the definition of the macros.
+************************************************************************************/ 
+float MQRead(int mq_pin)
+{
+  int i;
+  float rs=0;
+
+  for (i=0;i<READ_SAMPLE_TIMES;i++) {
+    rs += MQResistanceCalculation(analogRead(mq_pin));
+    delay(READ_SAMPLE_INTERVAL);
+  }
+
+  rs = rs/READ_SAMPLE_TIMES;
+
+  return rs;  
+}
+
+/*****************************  MQGetGasPercentage **********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         gas_id      - target gas type
+Output:  ppm of the target gas
+Remarks: This function uses different equations representing curves of each gas to 
+         calculate the ppm (parts per million) of the target gas.
+************************************************************************************/ 
+int MQGetGasPercentage(float rs_ro_ratio, int gas_id)
+{ 
+  if ( accuracy == 0 ) {
+  if ( gas_id == GAS_HYDROGEN ) {
+    return (pow(10,((-2.109*(log10(rs_ro_ratio))) + 2.983)));
+  } else if ( gas_id == GAS_LPG ) {
+    return (pow(10,((-2.123*(log10(rs_ro_ratio))) + 2.758)));
+  } else if ( gas_id == GAS_METHANE ) {
+    return (pow(10,((-2.622*(log10(rs_ro_ratio))) + 3.635)));
+  } else if ( gas_id == GAS_CARBON_MONOXIDE ) {
+    return (pow(10,((-2.955*(log10(rs_ro_ratio))) + 4.457)));
+  } else if ( gas_id == GAS_ALCOHOL ) {
+    return (pow(10,((-2.692*(log10(rs_ro_ratio))) + 3.545)));
+  } else if ( gas_id == GAS_SMOKE ) {
+    return (pow(10,((-2.331*(log10(rs_ro_ratio))) + 3.596)));
+  } else if ( gas_id == GAS_PROPANE ) {
+    return (pow(10,((-2.174*(log10(rs_ro_ratio))) + 2.799)));
+  }    
+} 
+
+  else if ( accuracy == 1 ) {
+    if ( gas_id == GAS_HYDROGEN ) {
+    return (pow(10,((-2.109*(log10(rs_ro_ratio))) + 2.983)));
+  } else if ( gas_id == GAS_LPG ) {
+    return (pow(10,((-2.123*(log10(rs_ro_ratio))) + 2.758)));
+  } else if ( gas_id == GAS_METHANE ) {
+    return (pow(10,((-2.622*(log10(rs_ro_ratio))) + 3.635)));
+  } else if ( gas_id == GAS_CARBON_MONOXIDE ) {
+    return (pow(10,((-2.955*(log10(rs_ro_ratio))) + 4.457)));
+  } else if ( gas_id == GAS_ALCOHOL ) {
+    return (pow(10,((-2.692*(log10(rs_ro_ratio))) + 3.545)));
+  } else if ( gas_id == GAS_SMOKE ) {
+    return (pow(10,(-0.976*pow((log10(rs_ro_ratio)), 2) - 2.018*(log10(rs_ro_ratio)) + 3.617)));
+  } else if ( gas_id == GAS_PROPANE ) {
+    return (pow(10,((-2.174*(log10(rs_ro_ratio))) + 2.799)));
+  }
+}    
+  return 0;
+}
